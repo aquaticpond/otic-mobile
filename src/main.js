8335ccfa23d799daf063abd329eab52947390cf7
Aquatic Pond <etheriad@gmail.com>
2017-11-26 21:05:49 -0500

    use the things

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
-// Import Vue
+
 import Vue from 'vue'
-
-// Import F7
+import Vuetify from 'vuetify'
 import Framework7 from 'framework7'
-
-// Import F7 Vue Plugin
 import Framework7Vue from 'framework7-vue'
 
+
 // Import F7 iOS Theme Styles
-import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
-/* OR for Material Theme:
+//import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
+//import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
+// OR for Material Theme:
  import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
  import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
- */
 
 // Import App Custom Styles
 import AppStyles from './assets/sass/main.scss'
+import FlexibleGridStyles from 'flexible-grid/dist/css/flexible-grid.css'
 
-// Import Routes
+
+
 import Routes from './routes.js'
-
-// Import App Component
 import App from './main.vue'
 
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue)
+Vue.use(Vuetify)
+
+window.hammer = require('hammerjs')
 
 // Init App
 new Vue({
